95f45672a60ebf0185e897097d687ee07ea5dd12
Murid Amrumiya <amrumiya_m@auca.kg>
2022-09-23 10:23:10 +0200

    Compile error in Problem02

--- Lab02/Problem02/tmpuva/main.cpp
+++ Lab02/Problem02/tmpuva/main.cpp
@@ -11,6 +11,27 @@ int main()
     iostream::sync_with_stdio(false);
 
 
+    int numTestCase;
+    cin >> numTestCase;
+    int row = 1;
+    int amount = 0;
+    int numOfSoldiers;
+    for(int i = 0; i < numTestCase; i++) {
+        cin >> numOfSoldiers;
+
+        int decrease = 1;
+        //TODO
+        while(numOfSoldiers != 1 || numOfSoldiers <= 0) {
+            numOfSoldiers -= decrease;
+            decrease++;
+            amount++;
+
+        }
+        cout << row;
+    }
+
+
+
 
 
 
