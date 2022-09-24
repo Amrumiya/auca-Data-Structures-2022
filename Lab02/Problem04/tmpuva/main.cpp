d237ce29d5669f6dc0c560c518d65db1e947f1c5
Murid Amrumiya <amrumiya_m@auca.kg>
2022-09-24 07:23:42 +0200

    Wrong answer in Problem04

---
+++ Lab02/Problem04/tmpuva/main.cpp
@@ -0,0 +1,33 @@
+
+#include <bits/stdc++.h>
+
+template <typename C>
+int sz(const C &c) { return static_cast<int>(c.size()); }
+
+using namespace std;
+
+int main()
+{
+    iostream::sync_with_stdio(false);
+
+
+    int numTestCase;
+    cin >> numTestCase;
+    for (int i = 0; i < numTestCase; i++) {
+        // int total[2];
+        int total;
+        int Num;
+        cin >> Num;
+        //int j = 0;
+        total = (((((Num * 567) / 9) + 7492) * 235) / 47) - 498;
+        // j++;
+        string toString = to_string(total);
+        int length = toString.length();
+        //cout << toString.substr(length - 2, 0, length - 1,length);
+         cout << toString[length - 2];
+       // printf("%c\n", toString[length - 2]);
+    }
+
+
+
+}
