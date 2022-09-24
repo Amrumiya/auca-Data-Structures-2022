95d488e50c67b5eb1a85c1f25565c46e180d6162
Murid Amrumiya <amrumiya_m@auca.kg>
2022-09-24 07:12:31 +0200

    Finish Problem03 from Lab02

---
+++ Lab02/Problem03/tmpuva/main.cpp
@@ -0,0 +1,24 @@
+
+#include <bits/stdc++.h>
+
+template <typename C>
+int sz(const C &c) { return static_cast<int>(c.size()); }
+
+using namespace std;
+
+int main()
+{
+    iostream::sync_with_stdio(false);
+
+    
+    int numTestCase;
+    cin >> numTestCase;
+
+    for (int i = 0; i < numTestCase; i++) {
+        int a, b;
+        cin >>a>>b;
+        int total = (a/3) * (b/3);
+    cout << total << endl;
+
+    }
+}
