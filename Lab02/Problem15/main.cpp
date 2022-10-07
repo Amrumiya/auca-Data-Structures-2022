c63e7a11811010cc9310d2bef8f8893fc030e78e
Murid Amrumiya <amrumiya_m@auca.kg>
2022-10-07 12:35:45 +0200

    Finish Problem15

---
+++ Lab02/Problem15/main.cpp
@@ -0,0 +1,41 @@
+
+#include <bits/stdc++.h>
+
+template <typename C>
+int sz(const C &c) { return static_cast<int>(c.size()); }
+
+using namespace std;
+
+int Search(vector<int> num) {
+    int max = 1;
+    for (int i = 0; i < (int) num.size(); i++) {
+        if (num[i] > max) max = num[i];
+    }
+    return max;
+}
+
+
+int main()
+{
+    iostream::sync_with_stdio(false);
+  int numTestCase;
+    cin >> numTestCase;
+
+    for (int i = 1; i <= numTestCase; i++) {
+
+        vector<string> text(10);
+        vector<int> rank(10);
+        for (int j = 0; j < 10; j++) {
+            cin >> text[j];
+            cin >> rank[j];
+        }
+
+        int max = Search(rank);
+
+        cout << "Case #" << i << ":" <<  endl;
+        for (int k = 0; k < (int) rank.size(); k++) {
+            if (rank[k] == max)
+                cout << text[k] << '\n';
+        }
+    }
+}
