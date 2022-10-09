849fa6d1da78a7eef05394b977eb77ac126b8596
Murid Amrumiya <amrumiya_m@auca.kg>
2022-10-09 07:36:37 +0200

    Wong answer in Problem16

--- Lab02/Problem16/main.cpp
+++ Lab02/Problem16/main.cpp
@@ -10,30 +10,39 @@ int main()
 {
     iostream::sync_with_stdio(false);
 
+int people;
+    cin >> people;
 
+    vector<string> NumOfPeople(people);
+    for (auto &e: NumOfPeople) {
+        cin >> e;
+    }
+    vector<bool> Checker(people);
     vector<string> messages(16);
-
     string birthday = " Happy birthday to you! Happy birthday to you! Happy birthday to Rujia! Happy birthday to you!!!";
-        
-      
+
     int j = 0;
     stringstream ssin(birthday);
-    while (ssin.good() && j < 16){
+    while (ssin.good() && j < 16) {
         ssin >> messages[j];
         ++j;
     }
+
     
-    int num;
-    cin>> num;
-    bool mom, girlfriend;
-    for(int i = 0; i < sz(messages); i++) {
-    if(i % 2 != 0) {
-        if(mom) {cout << "Mom" << messages[i] << '\n'; mom = false; girlfriend = true;}
-        if(girlfriend) {cout << "Girldfriend" << messages[i] << '\n'; girlfriend = false;
-        mom = true;}
+    Checker[0] = true;
+    for (int i = 1; i <= (int) messages.size(); i++) {
+        if (i % 2 != 0) {
+            if (Checker[0]) {
+                cout << NumOfPeople[0] << messages[i] << '\n';
+                Checker[0] = false;
+                Checker[2] = true;
+                Checker[1] = true;
+            } else if (Checker[2]) {
+                cout << NumOfPeople[2]<< ": " << messages[i] << '\n';
+                Checker[2] = false;
+                Checker[0] = true;
+            }
+        }
+        if (i % 2 == 0 && Checker[1]) {cout << NumOfPeople[1] << ": " << messages[i] << '\n'; Checker[1] = false; }
     }
-    if(i % 2 == 0) cout << "Dad" << messages[i] << '\n';    
-    }
-    
-    
 }   
