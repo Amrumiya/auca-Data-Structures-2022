fe9b596dc0c7bce6c6774190b477f25dc870e9ba
Murid Amrumiya <amrumiya_m@auca.kg>
2022-10-09 18:50:26 +0200

    Finish Problem17 from Lab02

--- Lab02/Problem17/main.cpp
+++ Lab02/Problem17/main.cpp
@@ -6,28 +6,29 @@ int sz(const C &c) { return static_cast<int>(c.size()); }
 
 using namespace std;
 
+
+
 int main()
 {
     iostream::sync_with_stdio(false);
 
-     int n, m;
-    char g[120][120];
-    while (scanf("%d%d", &n, &m) == 2) {
-        for (int i = 0; i < n; i++) scanf("%s", g[i]);
 
-        int answer = 0;
-        for (int i = 0; i < n; i++) {
-            bool checker = false;
-            for (int j = 0; j < m; j++) {
+    int firstNum, secondNum;
+    char g[150][150];
+
+    while (scanf("%d%d", &firstNum, &secondNum) == 2) {
+        for (int i = 0; i < firstNum; ++i) scanf("%s", g[i]);
+        int Answer = 0;
+        for (int i = 0; i < firstNum; ++i) {
+            bool Checker = false;
+            for (int j = 0; j < secondNum; ++j) {
                 if (g[i][j] == '/' || g[i][j] == '\\') {
-                    answer++;
-                    checker = !checker;
-                } else if (checker) answer += 2;
+                    ++Answer;
+                    Checker = !Checker;
+                } else if (Checker) Answer += 2;
             }
-            int FinalAnswer = answer / 2;
-            cout << FinalAnswer << '\n';
-
         }
+        cout << Answer / 2 << '\n';
     }
 }
 
