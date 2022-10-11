e69bd2364c311fe58671851129df1d788bba63f1
Murid Amrumiya <amrumiya_m@auca.kg>
2022-10-11 11:51:32 +0200

    Wrong answer in Problem18

---
+++ Lab02/Problem18/main.cpp
@@ -0,0 +1,52 @@
+
+#include <bits/stdc++.h>
+
+template <typename C>
+int sz(const C &c) { return static_cast<int>(c.size()); }
+
+using namespace std;
+
+int main()
+{
+    iostream::sync_with_stdio(false);
+
+    for(int nCourses; cin >> nCourses && nCourses != 0;) 
+    {
+        int nCategories;
+        cin >> nCategories;
+
+        vector<int> courses(nCourses);
+        for(auto &course : courses) 
+        {
+            cin >> course;
+        }
+
+        bool hasPassed = true;
+
+        for(int i = 0; i < nCategories; i++) {
+            int NumCourseCat;
+            cin >> NumCourseCat;
+            int mReq;
+            cin >> mReq;
+
+            vector<int> categCourses(NumCourseCat);
+            for(auto &course : categCourses) 
+            {
+                cin >> course;
+            }
+
+            sort(categCourses.begin(), categCourses.end());
+
+            int Take = 0;
+            for(auto course : courses)
+            {
+                Take+= binary_search(categCourses.begin(), categCourses.end(), course);
+            }
+        if(Take < mReq) {
+            hasPassed = false;
+        }
+        }
+        cout << (hasPassed ? "Yes\n" : "No\n");
+
+    }
+}
