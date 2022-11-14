487ce800cffb89f353eea3eef65702c93e4ac52b
Murid Amrumiya <amrumiya_m@auca.kg>
2022-11-14 03:51:56 +0100

    Error in one test case

--- project-01/tmpuva/BigInt.hpp
+++ project-01/tmpuva/BigInt.hpp
@@ -17,6 +17,36 @@ public:
 
         mDigits.push_back(0);
     }
+
+    BigInt(const std::string &value)
+        : mIsNegative(false)
+        {
+            if(value.empty())
+            {
+                throw std::runtime_error("BigInt : empty string");
+            }
+            size_t i = 0;
+            if(value[i] == '-' || value[i] == '+')
+            {
+                mIsNegative = value[i] == '-';
+                i++;
+            }
+
+            while(i + 1 != value.size() && value[i] == '0')
+            {
+                ++i;
+            }
+
+            for(; i != value.size(); i++)
+            {
+                if(!isdigit(value[i]))
+                {
+                    throw std::runtime_error("BigInt: Incorrect string value");
+                }
+                mDigits.push_back(value[i] - '0');
+            }
+        }
+
 };
 
 inline std::ostream &operator<<(std::ostream &out, const BigInt &x)
--- project-01/tmpuva/main.cpp
+++ project-01/tmpuva/main.cpp
@@ -26,10 +26,54 @@ TEST_CASE("Default constructor")
         REQUIRE(sout2.str() == "0");
 
     }
-
 }
 
+TEST_CASE("Constructor with string")
+{
+    ostringstream sout;
+    SUBCASE("Positive number") 
+    {
+    BigInt x("123456789123456789");
+    sout << x;
+    REQUIRE(sout.str() == "123456789123456789" );
+    }
+    SUBCASE("Negative number")
+    {
+        BigInt x("-123");
+        sout << x;
+        REQUIRE(sout.str() == "-123");
+    }
+    SUBCASE("Empty string")
+    {
+        REQUIRE_THROWS_AS(BigInt(""), runtime_error);
+    }
+    SUBCASE("+0")
+    {
+        BigInt x("0+");
+        sout << x;
 
+        REQUIRE(sout.str() == "0");
+    }
+    SUBCASE("Case -0")
+    {
+        BigInt x("-0");
+        sout << x;
+
+        REQUIRE(sout.str() == "0");
+    }
+    SUBCASE("Case 3")
+    {
+       REQUIRE_THROWS_AS(BigInt("-"), runtime_error);
+    }
+    SUBCASE("+")
+    {
+        REQUIRE_THROWS_AS(BigInt("+"), runtime_error);
+    }
+    SUBCASE("-")
+    {
+        REQUIRE_THROWS_AS(BigInt("-"), runtime_error);
+    }
+}
 
 
 
