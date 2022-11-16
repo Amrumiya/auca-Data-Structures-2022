fd73627b96eeca68eccc4dafaf1ccf1fe7416379
Murid Amrumiya <amrumiya_m@auca.kg>
2022-11-16 02:56:48 +0100

    Finish writing the < > operators with methods

--- project-01/tmpuva/BigInt.hpp
+++ project-01/tmpuva/BigInt.hpp
@@ -4,12 +4,29 @@
 
 class BigInt
 {
-   friend std::ostream &operator<<(std::ostream &out, const BigInt &x);
-   friend bool operator==(const BigInt &a, const BigInt &b);
-   friend bool operator!=(const BigInt &a, const BigInt &b);
-   friend bool operator>=(const BigInt &a, const BigInt &b);
-   friend bool operator<=(const BigInt &a, const BigInt &b);
-   friend bool operator>(const BigInt &a, const BigInt &b);
+
+    friend std::ostream &operator<<(std::ostream &out, const BigInt &x);
+    friend bool operator==(const BigInt &a, const BigInt &b);
+    friend bool operator!=(const BigInt &a, const BigInt &b);
+    friend bool operator>=(const BigInt &a, const BigInt &b);
+    friend bool operator<=(const BigInt &a, const BigInt &b);
+    friend bool operator<(const BigInt &a, const BigInt &b);
+
+    static int CompValue(const BigInt &a, const BigInt &b)
+    {
+        if (a.mDigits.size() > b.mDigits.size())
+            return 1;
+        if (a.mDigits.size() < b.mDigits.size())
+            return -1;
+        for (size_t i = 0; i < a.mDigits.size(); i++)
+        {
+            if (a.mDigits[i] != b.mDigits[i])
+            {
+                return a.mDigits[i] - b.mDigits[i];
+            }
+        }
+        return 0;
+    }
 
     std::vector<int> mDigits;
     bool mIsNegative;
@@ -24,33 +41,32 @@ public:
 
     BigInt(const std::string &value)
         : mIsNegative(false)
+    {
+        if (value.empty())
         {
-            if(value.empty())
-            {
-                throw std::runtime_error("BigInt : empty string");
-            }
-            size_t i = 0;
-            if(value[i] == '-' || value[i] == '+')
-            {
-                mIsNegative = value[i] == '-';
-                i++;
-            }
-
-            while(i + 1 != value.size() && value[i] == '0')
-            {
-                ++i;
-            }
-
-            for(; i != value.size(); i++)
-            {
-                if(!isdigit(value[i]))
-                {
-                    throw std::runtime_error("BigInt: Incorrect string value");
-                }
-                mDigits.push_back(value[i] - '0');
-            }
+            throw std::runtime_error("BigInt : empty string");
+        }
+        size_t i = 0;
+        if (value[i] == '-' || value[i] == '+')
+        {
+            mIsNegative = value[i] == '-';
+            i++;
         }
 
+        while (i + 1 != value.size() && value[i] == '0')
+        {
+            ++i;
+        }
+
+        for (; i != value.size(); i++)
+        {
+            if (!isdigit(value[i]))
+            {
+                throw std::runtime_error("BigInt: Incorrect string value");
+            }
+            mDigits.push_back(value[i] - '0');
+        }
+    }
 };
 
 inline std::ostream &operator<<(std::ostream &out, const BigInt &x)
@@ -66,7 +82,17 @@ inline std::ostream &operator<<(std::ostream &out, const BigInt &x)
     return out;
 }
 
-bool operator==(const BigInt &a, const BigInt &b) 
+inline bool operator<(const BigInt &a, const BigInt &b)
+{
+    if (a.mIsNegative && !(b.mIsNegative))
+        return true;
+    if (!(a.mIsNegative) && b.mIsNegative)
+        return false;
+    if (!(a.mIsNegative) && (b.mIsNegative))
+        return (BigInt::CompValue(a, b) > 0);
+}
+
+bool operator==(const BigInt &a, const BigInt &b)
 {
     return a.mIsNegative == b.mIsNegative && a.mDigits == b.mDigits;
 }
@@ -88,9 +114,13 @@ inline bool operator>=(const BigInt &a, const BigInt &b)
 
 inline bool operator>(const BigInt &a, const BigInt &b)
 {
-    if(a == b)
+    if (a == b)
     {
         return false;
     }
     return !(a < b);
+}
+inline bool operator<(const BigInt &a, const BigInt &b)
+{
+    return !(b < a);
 }
