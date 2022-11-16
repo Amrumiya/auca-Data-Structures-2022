5674afe0667ed6412ec571efe3adfec30ec94162
Murid Amrumiya <amrumiya_m@auca.kg>
2022-11-16 03:12:40 +0100

    Compile error (redifinition of < op)

--- project-01/tmpuva/main.cpp
+++ project-01/tmpuva/main.cpp
@@ -8,7 +8,7 @@ int sz(const C &c) { return static_cast<int>(c.size()); }
 
 using namespace std;
 
-
+/*
 TEST_CASE("Default constructor")
 {
     BigInt x;
@@ -61,23 +61,46 @@ TEST_CASE("Constructor with string")
 
         REQUIRE(sout.str() == "-0");
     }
- /*  SUBCASE("Case 3")
-    {
-       REQUIRE_THROWS_AS(BigInt("-"), runtime_error);
-    } */
-/*    SUBCASE("+")
-    {
-        REQUIRE_THROWS_AS(BigInt("+"), runtime_error);
-    }
-    SUBCASE("-")
-    {
-        REQUIRE_THROWS_AS(BigInt("-"), runtime_error);
-    }*/
-  /*   SUBCASE("12344d132")
-    {
-        REQUIRE_THROWS_AS(BigInt("-"), runtime_error);
-    }*/
 }
+*/
+TEST_CASE("Greater or Equal") 
+{
+    ostringstream sout;
+    SUBCASE("-10 and -10")
+    {
+        BigInt x("-10");
+        BigInt y("-10");
+
+        REQUIRE(x >= y);
+    }
+    SUBCASE("-10 and 10")
+    {
+        BigInt x("10");
+        BigInt y("-10");
+
+        REQUIRE(x >= y);
+    }
+}
+
+TEST_CASE("Less than or equal ")
+{
+    ostringstream sout;
+    SUBCASE("-100 and 100")
+    {
+        BigInt x("100");
+        BigInt y("-100");
+
+        REQUIRE(y <= x);
+    }
+    SUBCASE("-5 and 4")
+    {
+        BigInt x("-5");
+        BigInt y("4");
+
+        REQUIRE(x <= y);
+    }
+}
+
 
 
 
