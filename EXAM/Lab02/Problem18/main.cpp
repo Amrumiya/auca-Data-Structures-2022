c57f15bcbbce7e1ad8adc19631ff96244f930dd4
Murid Amrumiya <amrumiya_m@auca.kg>
2022-12-22 18:45:09 +0100

    commit

--- EXAM/Lab02/Problem18/main.cpp
+++ EXAM/Lab02/Problem18/main.cpp
@@ -12,16 +12,18 @@ int main()
   int t;
   while (cin >> t)
   {
+    cin.ignore();
     for (int i = 0; i < t; i++)
     {
       cin >> name;
       names.push_back(name);
     }
+
     stable_sort(begin(names), end(names), [](string first, string second)
                 {
-  if(first[0] != second[0])
-  return first[0] < second[0];
-  else return first[1] < second[1]; });
+      if(first[0] != second[0])
+      return first[0] < second[0];
+      else return first[1] < second[1]; });
 
     for (auto &i : names)
     {
